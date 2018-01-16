63f7d9adf4ab56a388ff86576d81f6a796413110
jencekade <gaephuoc@gmail.com>
2018-01-16 02:34:57 +0000

    automatically backed up by learn

--- looping.js
+++ looping.js
@@ -4,7 +4,7 @@ console.log(names[0])
 console.log(names[1])
 console.log(names[2])
 
-for(i = 0; i < names.length; i++, ) {
+for(i = 0; i < names.length; i++ ) {
   console.log(name)
 }
 
@@ -19,6 +19,23 @@ while (i <= 10) {
   i++
 }
 
+/*
+While loops will execute the looop body if the condition is true
+*/
+
 [1, 2, 3].forEach(function(element) {
   console.log(element)
 }
+
+function loop25Times(array){
+  for(var i=1; i< 26; i++){
+    array.push(`I am step number ${i} of the loop`);
+  }
+  return array;
+}
+
+/*
+I don't understand why the instructions say to return
+the array that contains the 25 new statements.
+I feel that just console logging the statements should suffice.
+*/
