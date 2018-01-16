461828dc450fb0ae787e396c44ffadb697e19a53
jencekade <gaephuoc@gmail.com>
2018-01-16 02:20:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -22,3 +22,12 @@ Object.assign({}, course, {start_date: "2017-01-01"})
 delete course["title"]
 
 Object.assign({}, course, {description: "a kick-ass introduction to JS"})
+
+
+
+/*
+Object.assign takes in 3 arguments
+1) a new empty object
+2) the name of the variable assigned to an existing object
+3) the key value pairs (more than 1 can be added)
+*/
